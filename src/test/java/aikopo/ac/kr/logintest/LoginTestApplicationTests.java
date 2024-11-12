06ba32dd71ca3c3f13d95c6d7d0041913ea5e711
00seungho <tmdgh9910@naver.com>
2024-11-12 16:18:40 +0900

    spring시큐리티 커스터마이징

---
+++ src/test/java/aikopo/ac/kr/logintest/LoginTestApplicationTests.java
@@ -0,0 +1,13 @@
+package aikopo.ac.kr.logintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
